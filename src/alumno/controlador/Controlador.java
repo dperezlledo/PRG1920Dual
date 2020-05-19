90e9062248ecd357f8707147d39a6701575b5fdb
dperezlledo <dperez.comercio@gmail.com>
2020-05-19 16:53:52 +0200

    Tratamiento de errores básicos de una BD en el patrón MVC con excepciones

--- src/alumno/controlador/Controlador.java
+++ src/alumno/controlador/Controlador.java
@@ -21,10 +21,10 @@ import java.util.logging.Logger;
  */
 public class Controlador implements ActionListener {
 
-    private IDAO modelo;
-    private IVista vista;
+    private IDAO<Alumno> modelo;
+    private IVista<Alumno> vista;
 
-    public Controlador(IVista vista, IDAO modelo) {
+    public Controlador(IVista<Alumno> vista, IDAO<Alumno> modelo) {
         this.vista = vista;
         this.modelo = modelo;
     }
@@ -34,30 +34,31 @@ public class Controlador implements ActionListener {
         Alumno aux, aux2;
         boolean flag;
         if (e.getActionCommand().equals("Siguiente")) {
-            aux = (Alumno) vista.getObjeto();
-            aux2 = (Alumno) modelo.siguiente(aux);
+            aux = vista.getObjeto();
+            aux2 = modelo.siguiente(aux);
             vista.setObjeto(aux2);
         } else if (e.getActionCommand().equals("Primero")) {
-            aux2 = (Alumno) modelo.primero();
+            aux2 = modelo.primero();
             vista.setObjeto(aux2);
         } else if (e.getActionCommand().equals("Anterior")) {
-            aux2 = (Alumno) vista.getObjeto();
-            aux = (Alumno) modelo.anterior(aux2);
+            aux2 = vista.getObjeto();
+            aux = modelo.anterior(aux2);
             vista.setObjeto(aux);
         } else if (e.getActionCommand().equals("Ultimo")) {
-            aux = (Alumno) modelo.ultimo();
+            aux = modelo.ultimo();
             vista.setObjeto(aux);
         } else if (e.getActionCommand().equals("Alta")) {
             try {
-                aux = (Alumno) vista.getObjeto();
-                if (!modelo.alta(aux))
+                aux = vista.getObjeto();
+                if (!modelo.alta(aux)) {
                     vista.muestraMensaje("No es posible realizar la operación");
+                }
             } catch (Exception ex) {
                 vista.muestraMensaje(ex.getMessage());
-            }            
-            
+            }
+
         } else if (e.getActionCommand().equals("Baja")) {
-            aux = (Alumno) vista.getObjeto();
+            aux = vista.getObjeto();
             flag = modelo.baja(aux);
             if (flag) {
                 vista.muestraMensaje("Baja realizada");
@@ -67,8 +68,8 @@ public class Controlador implements ActionListener {
         } else if (e.getActionCommand().equals("Modificar")) {
 
         } else if (e.getActionCommand().equals("Consulta")) {
-            aux = (Alumno) vista.getObjeto();
-            aux = (Alumno) modelo.consulta(aux.getId());
+            aux = vista.getObjeto();
+            aux = modelo.consulta(aux.getId());
             if (aux == null) {
                 vista.muestraMensaje("No existe el alumno");
             } else {
